9755a12d9da6839026e9659536b58670e3e6ec92
Daniel <in.lieu.of.leu@gmail.com>
2023-08-22 21:51:57 +0100

    Fix background

--- content-nextjs/src/pages/index.tsx
+++ content-nextjs/src/pages/index.tsx
@@ -8,23 +8,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width" />
       </Head>
 
-      <div className="navBar">
-      <h1><a href="/">About Me</a> - <a href="/projects">My Projects</a> - <a href="/recommended">What I've Learned</a></h1>
-      </div>
-      <div className="body">
-      <p>Hello 👋, I'm Daniel. I'm studying Computer Science in my 3rd year at Imperial College London.</p><p>You can contact me through:</p>
-      <ul>
-        <li>Discord:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;inlieuofleu</li>
-        <li>Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;in.lieu.of.leu@gmail.com</li>
-        <li>Uni Email:&nbsp;&nbsp;ds1920@ic.ac.uk</li>
-        <li>Number:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+44 7340 364 913</li>
-      </ul>
-      <p>And here are some professional links:</p>
-      <ul>
-        <li><a href="https://www.linkedin.com/in/daniel-simols-325325207/">LinkedIn</a></li>
-        <li><a href="https://github.com/leu">GitHub</a></li>
-        <li><a href="/data/cv.pdf">CV</a></li>
-      </ul>
+      <div className="background">
+        <div className="navBar">
+          <h1><a href="/">About Me</a> - <a href="/projects">My Projects</a> - <a href="/recommended">What I've Learned</a></h1>
+        </div>
+        <div className="body">
+          <p>Hello 👋, I'm Daniel. I'm studying Computer Science in my 3rd year at Imperial College London.</p><p>You can contact me through:</p>
+          <ul>
+            <li>Discord:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;inlieuofleu</li>
+            <li>Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;in.lieu.of.leu@gmail.com</li>
+            <li>Uni Email:&nbsp;&nbsp;ds1920@ic.ac.uk</li>
+            <li>Number:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+44 7340 364 913</li>
+          </ul>
+          <p>And here are some professional links:</p>
+          <ul>
+            <li><a href="https://www.linkedin.com/in/daniel-simols-325325207/">LinkedIn</a></li>
+            <li><a href="https://github.com/leu">GitHub</a></li>
+            <li><a href="/data/cv.pdf">CV</a></li>
+          </ul>
+        </div>
       </div>
     </>
   )
--- content-nextjs/src/pages/projects/index.tsx
+++ content-nextjs/src/pages/projects/index.tsx
@@ -8,14 +8,16 @@ export default function Projects() {
 				<meta name="viewport" content="width=device-width" />
 			</Head>
 
-			<div className="navBar">
-			<h1><a href="/">About Me</a> - <a href="/projects">My Projects</a> - <a href="/recommended">What I've Learned</a></h1>
-			</div>
+			<div className="background">
+				<div className="navBar">
+				<h1><a href="/">About Me</a> - <a href="/projects">My Projects</a> - <a href="/recommended">What I've Learned</a></h1>
+				</div>
 
-			<div className="body">
-				<h2>The Scrawler</h2>
-				<img src="/tif.jpg" width="800px" /><br />
-				<p><a className="font-size: 26px; center" href="https://github.com/leu/tif">https://github.com/leu/tif</a></p>
+				<div className="body">
+					<h2>The Scrawler</h2>
+					<img src="/tif.jpg" width="800px" /><br />
+					<p><a className="font-size: 26px; center" href="https://github.com/leu/tif">https://github.com/leu/tif</a></p>
+				</div>
 			</div>
 		</>
 	)
