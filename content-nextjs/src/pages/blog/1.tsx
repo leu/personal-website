866d49270c8107301afe1128728a8f6eb1975c47
Daniel <in.lieu.of.leu@gmail.com>
2023-08-23 15:42:36 +0100

    Update 'Take The Easy Way' to 'Laziness Is A Tool', update contents a bit

--- content-nextjs/src/pages/blog/1.tsx
+++ content-nextjs/src/pages/blog/1.tsx
@@ -5,14 +5,14 @@ export default function Blog1() {
 	return (
 		<>
 			<Head>
-				<title>Daniel Simols - Take The Easy Way</title>
+				<title>Daniel Simols - Laziness Is A Tool</title>
 				<meta name="viewport" content="width=device-width" />
 			</Head>
 
             <div className="background">
                 {Navbar()}
                 <div className="body">
-                    <h1>Take The Easy Way</h1>
+                    <h1>Laziness Is A Tool</h1>
                     <p>One thing that’s constantly on my mind is 
                         “Why am I so lazy? Why don’t I feel like doing this simple task even 
                         though I would simply benefit?”</p>
@@ -28,15 +28,27 @@ export default function Blog1() {
                         mind is OK with spending time on, maybe we shouldn’t call that ‘laziness’, 
                         but something like ‘pragmatic’! Maybe it’s a good thing that our mind pushes
                         us to consider “Maybe there’s a better use of my time”.</p>
-                    <p>Though, I guess I wouldn’t go so far as to say watching Netflix is 
-                        objectively good for you, which a lot of people do, but perhaps that’s 
-                        because of the shift in how deadly our environment is compared to the past. 
-                        Maybe humanity will very slowly shift to being less ‘lazy’. I don’t know, 
-                        I’m not a biologist or anything like that.</p>
-                    <p>Either way, if you want to combat this feeling of laziness, maybe it would be 
-                        best to listen to it and to consider the question “What’s the easiest way of 
-                        doing this?” in all your matters. Don’t unnecessarily waste your time.</p>
-                    <p>2023/08/22</p>
+                    <p>Though, I guess I wouldn’t go so far as to say watching Netflix is
+                        objectively good for you, which a lot of people do. But it makes sense 
+                        that as organisms we don’t want to pursue something if we already feel 
+                        secure enough. Perhaps a lot of it is how our environment influences us, 
+                        or habit. I don’t really know.
+                    </p>
+                    <p>
+                        Either way, I’ve found that asking myself “What’s the easiest way of 
+                        doing this” actually helps me to achieve goals while dealing with this feeling 
+                        of laziness. I realised that at times I’d been stubbornly thinking that I’m 
+                        simply lazy for dreading doing a task, when in reality it was my body 
+                        telling me “Hey, maybe you could do this task in a more efficient way” 
+                        rather than it being something like divine retribution for my hubris.
+                    </p>
+                    <p>
+                        If you’re feeling lazy, consider if what you’re doing is really worth your time. 
+                        Use your laziness as a tool to help yourself stay efficient.
+                    </p>
+                    <p>
+                        2023/08/22
+                    </p>
                 </div>
             </div>
 		</>
