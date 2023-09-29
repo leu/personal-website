efb1f6fa7ea1c1f55cfc530c48e2fe3bff5a75bd
Daniel <in.lieu.of.leu@gmail.com>
2023-09-29 12:35:42 +0100

    Update /recommended (What I've Learned) to My Advice For You, and do boredom instead of standing desk

--- content-nextjs/src/components/navbar.tsx
+++ content-nextjs/src/components/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
             <h1>
                 <a href="/">About Me</a> 
                 &nbsp;- <a href="/scrawler">Scrawler</a> 
-                &nbsp;- <a href="/recommended">What I've Learned</a>
+                &nbsp;- <a href="/advice">My Advice For You</a>
                 &nbsp;- <a href="/blog">Blog</a>
             </h1>
         </div>)
---
+++ content-nextjs/src/pages/advice/index.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Navbar from "../../components/navbar";
+
+export default function Recommended() {
+	return (
+		<>
+			<Head>
+				<title>Daniel Simols - Advice</title>
+				<meta name="viewport" content="width=device-width" />
+			</Head>
+
+            <div className="background">
+                {Navbar()}
+                <div className="body">
+                    <h1>My Advice For You</h1>
+                    <h2>Boredom</h2>
+                    <p>
+                        I believe one of the healthiest things you can do is get yourself bored. Feel like you're not living your life to the fullest? Boredom will put you on the right track.
+                    </p>
+                    <h2>Journalling</h2>
+                    <p>
+                        Writing daily in my journal is probably the most helpful thing I've learned to do. You can't really think without writing it down. It also really helps with going through with doing things differently in your life. I highly recommend trying it.
+                    </p>
+                </div>
+            </div>
+		</>
+	)
+}
