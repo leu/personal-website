545f61e3918faf81dbec39d529dfbf075b3cac69
Daniel <in.lieu.of.leu@gmail.com>
2023-09-29 14:51:51 +0100

    Update the icon to my self-portrait

--- content-nextjs/src/pages/_document.tsx
+++ content-nextjs/src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/icon.ico" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/self-portrait.jpeg" />
       </Head>
       <body>
         <Main />
