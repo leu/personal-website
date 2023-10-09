26368d10d69fb6873f7985445d0a04272e396305
Daniel <in.lieu.of.leu@gmail.com>
2023-10-09 21:25:27 +0100

    Rebrand 'Boredom' to 'Cutting Off Distractions'

--- content-nextjs/src/pages/advice/index.tsx
+++ content-nextjs/src/pages/advice/index.tsx
@@ -13,9 +13,12 @@ export default function Recommended() {
                 {Navbar()}
                 <div className="body">
                     <h1>My Advice</h1>
-                    <h2>Boredom</h2>
+                    <h2>Cutting Off Distractions</h2>
                     <p>
-                        I believe one of the healthiest things you can do is get yourself bored. Feel like you're not living your life to the fullest? Boredom will put you on the right track.
+                        I believe one of the healthiest things you can do is cut off mindless entertainment. 
+                        Things like social media apps, youtube, netflix, etc.
+                        This will get you bored which will motivate you to pursue whatever you want to pursue.
+                        I believe this is actually a way to achieve self-actualisation.
                     </p>
                     <h2>Journalling</h2>
                     <p>
