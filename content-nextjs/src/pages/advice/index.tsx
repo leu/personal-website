13fd467a20e1d05a1b2f5fb0350e5bff83616791
Daniel <in.lieu.of.leu@gmail.com>
2023-10-22 14:03:52 +0100

    In advice, remove Journalling, and update 'Cutting Off Distractions' to 'Avoid Bread and Circuses'

--- content-nextjs/src/pages/advice/index.tsx
+++ content-nextjs/src/pages/advice/index.tsx
@@ -13,16 +13,12 @@ export default function Recommended() {
                 {Navbar()}
                 <div className="body">
                     <h1>My Advice</h1>
-                    <h2>Cutting Off Distractions</h2>
+                    <h2>Avoid Bread and Circuses</h2>
                     <p>
                         I believe one of the healthiest things you can do is cut off mindless entertainment. 
                         Things like social media apps, youtube, netflix, etc.
                         This will get you bored which will motivate you to pursue whatever you want to pursue.
                         I believe this is actually a way to achieve self-actualisation.
-                    </p>
-                    <h2>Journalling</h2>
-                    <p>
-                        Writing daily in my journal is probably the most helpful thing I've learned to do. You can't really think without writing it down. It also really helps with going through with doing things differently in your life. I highly recommend trying it.
                     </p>
                     <img className="vagabondImage" src="/large-mountains.jpg"/>
                 </div>
