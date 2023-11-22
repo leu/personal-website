ef6571f7b89f11d753d3fd996459d9c7957d0aed
Daniel <in.lieu.of.leu@gmail.com>
2023-11-22 09:37:13 +0000

    Update 'My Advice' with more rational take on Boredom, and add 'Sometimes You Just Need To Force Yourself'

--- content-nextjs/src/pages/advice/index.tsx
+++ content-nextjs/src/pages/advice/index.tsx
@@ -17,10 +17,22 @@ export default function Recommended() {
                     <p>
                         I believe one of the healthiest things you can do is cut off mindless entertainment. 
                         Things like social media apps, youtube, netflix, etc.
-                        This will get you bored which will motivate you to pursue whatever you want to pursue.
-                        I believe this is actually a way to achieve self-actualisation.
+                        The reasons are twofold. One, I feel depressed after some time on social media.
+                        Two, it's addicting and indirectly stops you from achieving more rewarding goals.
                     </p>
                     <img className="vagabondImage" src="/large-mountains.jpg"/>
+                    <h2>Sometimes You Just Need to Force Yourself</h2>
+                    <p>
+                        I like to find ways to make things easier, rather than trying harder. Study smart, not hard etc.
+                        So I cut off a lot of the useless things from my life (social media, mindless games, etc), and
+                        now I'm left with a lot of free time. And this has got me far! But this doesn't automatically go
+                        into pursuing fulfilling goals.
+                        It's not uncommon for me to sleep for over 12 hours some days. So there are times I get depressed.
+                        And so I've realised that actually <em>trying</em> is necessary for happiness. It may sound obvious,
+                        but it's hard, really hard, so it's easy to forget. I understand now what various authors mean when they 
+                        write that work is the best medicine.
+                    </p>
+                    <img className="vagabondImage" src="/vagabond-alone.jpg"/>
                 </div>
             </div>
 		</>
